5c5e07e87b4bd4e8f2ebe20ed7a1574107d3dd1b
Nath <nathaan59@gmail.com>
2025-02-14 16:29:54 +0100

    test axios

--- src/page/CrowdshipperList.jsx
+++ src/page/CrowdshipperList.jsx
@@ -8,7 +8,7 @@ const CrowdshipperList = () => {
 
     useEffect(() => {
         // Fetch data from the API
-        axios.get('http://localhost:8080/api/crowdshippers')
+        axios.get('http://xn--connects-h1a.fr:8885/api/crowdshippers')
             .then(response => {
                 setData(response.data);
             })
